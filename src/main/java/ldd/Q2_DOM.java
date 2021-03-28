36e4ccdf386f6b3f777874cd004c18f5ec12e7cc
Leonardo <leosoutomaior@hotmail.com>
2021-03-28 20:46:38 -0300

    completo

--- src/main/java/ldd/Q2_DOM.java
+++ src/main/java/ldd/Q2_DOM.java
@@ -9,14 +9,16 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import static java.util.Collections.list;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -28,30 +30,57 @@ import org.xml.sax.SAXException;
  */
 public class Q2_DOM {
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerConfigurationException, TransformerException {
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new File("albums.xml"));
         Document q2_output = db.newDocument();
-        
+
         Element ol = q2_output.createElement("ol");
         q2_output.appendChild(ol);
 
-        Map<String, String> map = new HashMap<>();
+        List<Ordenar> list = new ArrayList<>();
+        
 
         NodeList songs = doc.getElementsByTagName("song");
         for (int i = 0; i < songs.getLength(); i++) {
             Element ele = (Element) songs.item(i);
-            map.put(ele.getAttribute("title"), ele.getAttribute("length"));
-
+            Ordenar text = new Ordenar();
+            text.length = ele.getAttribute("length");
+            text.album = ele.getAttribute("title");
+            list.add(text);
         }
-        
-        Object[] orderedMap
-                = map.entrySet().stream().sorted(Map.Entry.comparingByValue()).toArray();
-        for
 
-        //System.out.println(map);
+        Collections.sort(list);
+
+        for (Ordenar text : list) {
+            String item = text.album + " (" + text.length + ")";
+            Element li = q2_output.createElement("li");
+            li.setTextContent(item);
+            ol.appendChild(li);
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(q2_output);
+        StreamResult result = new StreamResult(new File("q2_output.xml"));
+        transformer.transform(source, result);
+    }
+    
+    public static class Ordenar implements Comparable<Ordenar> {
+
+        private String album;
+        private String length;
+
+        @Override
+        public int compareTo(Ordenar o) {
+            int valor = length.compareTo(o.length) * -1;
+            if (valor == 0) {
+                return album.compareTo(o.album);
+            }
+            return valor;
+        }
     }
 
 }
--- src/main/java/ldd/Q3_SAX.java
+++ src/main/java/ldd/Q3_SAX.java
@@ -5,10 +5,114 @@
  */
 package ldd;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 /**
  *
  * @author Dev Dreamm
  */
-public class Q3_SAX {
-    
+public class Q3_SAX extends DefaultHandler {
+
+    private String album;
+    private int qtdSong;
+    private List<Teste> list = new ArrayList<>();
+
+    public List<Teste> getList() {
+        return list;
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        Collections.sort(list);
+    }
+
+    @Override
+    public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+
+        if (qName.equals("album")) {
+            album = atts.getValue(0);
+            qtdSong = 0;
+        } else if (qName.equals("song")) {
+            qtdSong++;
+        }
+    }
+
+    @Override
+    public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+        if (qName.equals("album")) {
+
+            Teste albumFaixa = new Teste();
+            albumFaixa.album = album;
+            albumFaixa.qtd = qtdSong;
+            list.add(albumFaixa);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, XMLStreamException {
+        File inputFile = new File("albums.xml");
+        Q3_SAX userhandler = new Q3_SAX();
+
+        XMLOutputFactory xmlof = XMLOutputFactory.newFactory();
+        Writer writer = new FileWriter("q3_output.xml");
+        XMLStreamWriter xmlsw = xmlof.createXMLStreamWriter(writer);
+
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(inputFile, userhandler);
+
+            //Inicio do documento
+            xmlsw.writeStartDocument();
+            xmlsw.writeStartElement("table");
+            List<Teste> list2 = userhandler.getList();
+            for (Teste string : list2) {
+                xmlsw.writeStartElement("tr");
+                xmlsw.writeStartElement("td");
+                xmlsw.writeCharacters(string.album);
+                xmlsw.writeEndElement();
+                xmlsw.writeStartElement("td");
+                xmlsw.writeCharacters(string.qtd.toString());
+                xmlsw.writeEndElement();
+                xmlsw.writeEndElement();
+            }
+            xmlsw.writeEndElement();
+            xmlsw.writeEndDocument();
+            xmlsw.close();
+            //Fim do documento
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public class Teste implements Comparable<Teste> {
+
+        private String album;
+        private Integer qtd;
+
+        @Override
+        public int compareTo(Teste o) {
+            int valor = qtd.compareTo(o.qtd) * -1;
+            if (valor == 0) {
+                return album.compareTo(o.album);
+            }
+            return valor;
+        }
+    }
+
 }
